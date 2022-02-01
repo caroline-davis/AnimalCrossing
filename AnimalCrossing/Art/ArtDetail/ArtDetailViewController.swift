a08aee7a654d0389f183ae6f4ff090c7e0d1edc3
Caroline Davis <caroline_davis@live.com>
2022-02-01 15:55:00 +1100

    feat: Add data to details page

--- AnimalCrossing/Art/ArtDetail/ArtDetailViewController.swift
+++ AnimalCrossing/Art/ArtDetail/ArtDetailViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ArtDetailViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var artName: UILabel!
+    @IBOutlet weak var artDescription: UILabel!
+    @IBOutlet weak var buyPrice: UILabel!
+    @IBOutlet weak var sellPrice: UILabel!
+    @IBOutlet weak var hasFake: UILabel!
+
     var artDetailViewModel: ArtDetailViewModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = false
+
+        setInitialItems()
+
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.navigationController?.isNavigationBarHidden = true
+    }
+
+    func setInitialItems() {
+        artName.text = artDetailViewModel?.name
+        artDescription.text = artDetailViewModel?.imageDescription
+        hasFake.text = artDetailViewModel?.hasFake
+        buyPrice.text = artDetailViewModel?.buyPrice
+        sellPrice.text = artDetailViewModel?.sellPrice
+
     }
 
 
--- AnimalCrossing/Art/ArtDetail/ArtDetailViewModel.swift
+++ AnimalCrossing/Art/ArtDetail/ArtDetailViewModel.swift
@@ -16,5 +16,24 @@ class ArtDetailViewModel {
         self.selectedItem = selectedItem
     }
 
+    var hasFake: String {
+        return "Has fake: \(selectedItem.hasFake)"
+    }
+
+    var name: String {
+        return selectedItem.name["name-USen"]?.capitalized ?? ""
+    }
+
+    var buyPrice: String {
+        return "Buy price: \(selectedItem.buyPrice)"
+    }
+
+    var sellPrice: String {
+        return "Sell price: \(selectedItem.sellPrice)"
+    }
+
+    var imageDescription: String {
+        return selectedItem.imageDescription
+    }
 
 }
